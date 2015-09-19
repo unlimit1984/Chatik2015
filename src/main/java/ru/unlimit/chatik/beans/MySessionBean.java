c6d460496f5484c1873ab11843022f25903802ff
Vladimir Vysokomorny <unlimit_rmc@mail.ru>
2015-09-20 02:22:18 +0300

    changing

---
+++ src/main/java/ru/unlimit/chatik/beans/MySessionBean.java
@@ -0,0 +1,20 @@
+package ru.unlimit.chatik.beans;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class MySessionBean
+ */
+@Stateless
+@LocalBean
+public class MySessionBean {
+
+    /**
+     * Default constructor. 
+     */
+    public MySessionBean() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
